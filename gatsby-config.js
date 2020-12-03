8ded92cd533d470d2c11e983435d4fde3aec6c5a
Alex Merced <willcodeforfood@alexmerced.com>
2020-12-03 17:46:44 -0500

    almost there

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,25 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: "My New Blog",
+    description: "This is my awesome blog I made from scratch!",
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown`,
+        path: `${__dirname}/src/mark/`,
+      },
+    },
+    `gatsby-transformer-remark`,
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,37 @@
 import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../layout/Layout"
 
-export default function Home() {
-  return <div>Hello world!</div>
+export default function Blog({ data }) {
+  const { posts } = data.blog
+
+  return (
+    <Layout>
+      <h1>My blog posts</h1>
+
+      {posts.map(post => (
+        <article key={post.id}>
+          <h2>post.frontmatter.title</h2>
+          <small>{post.frontmatter.author}, {post.frontmatter.date}</small>
+          <p>{post.excerpt}</p>
+        </article>
+      ))}
+    </Layout>
+  )
 }
+
+export const pageQuery = graphql`
+  query MyQuery {
+    blog: allMarkdownRemark {
+      posts: nodes {
+        frontmatter {
+          date(fromNow: true)
+          title
+          author
+        }
+        excerpt
+        id
+      }
+    }
+  }
+`
